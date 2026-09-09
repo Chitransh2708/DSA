06badf4135d32f277749258c66c22c660cea2b42
Chitransh Yadav <chitranshy6@gmail.com>
2026-09-09 20:26:41 +0530

    Time: 4 ms (36.41%) | Memory: 43.4 MB (40.76%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
         
-        
 
         Stack<Character> st = new Stack<>();
         for(int i =0;i<=s.length()-1;i++){
@@ -12,30 +11,41 @@ class Solution {
             else if(c == '{' || c=='[' || c=='('){
                 st.push(c);
             }
-            else{ 
-                  if (st.empty()) {
+            else if(c == ')'){
+                if(st.peek() == '('){
+                    st.pop();
+                }
+                else{
                     return false;
                 }
-
-                if (c == ')' && st.peek() != '(') {
-                    return false;
-                }
-                if (c == '}' && st.peek() != '{') {
-                    return false;
-                }
-                if (c == ']' && st.peek() != '[') {
-                    return false;
-                }
-
-                st.pop();
-            }
                 
+            }
+            else if(c == '}'){
+                if(st.peek() == '{'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+                
+            }
+            else if(c == ']'){
+                if(st.peek() == '['){
+                    st.pop();
+                }
+
+                else{
+                    return false;
+                }
+                
+            }
         }
-        
-        
-
-
-        return st.empty();
+        if(st.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
 
 
         
