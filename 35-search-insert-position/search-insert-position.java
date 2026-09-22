68b025c81abc38673957f3418a988416bc690655
Chitransh Yadav <chitranshy6@gmail.com>
2026-09-22 12:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (82.94%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+        int low = 0;
+        int res = nums.length;
+        int high = nums.length-1;
+        while(low<=high){
+            int guess=(low+high)/2;
+
+            if(nums[guess]==target){
+                return guess;
+            }
+            else if (nums[guess]>target){
+                res = guess;
+                high = guess-1;
+            }
+            else{
+                low = guess+1;
+            }
+        }
+        return res;
+    }
+}
